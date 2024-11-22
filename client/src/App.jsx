6e72590c8b5dfead290269f3c2bae0b4e59cd79f
Logan Bunch <loganbunch@gmail.com>
2024-11-21 20:09:54 -0800

    Thu Nov 21 20:09:54 PST 2024

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import Login from "./components/Login.jsx";
 import { AuthContext } from "./lib/AuthContext.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useContext } from "react";
 import FpApp from "./FpApp.jsx";
+import Dump from "./Dump.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FpApp />,
+  },
+  {
+    path: "/dump",
+    element: <Dump />,
+  },
+]);
 
 export default function App() {
   const { isAuthenticated, user } = useContext(AuthContext);
@@ -11,7 +24,7 @@ export default function App() {
       {isAuthenticated
         ? (
           <>
-            <FpApp />
+            <RouterProvider router={router} />
           </>
         )
         : (
